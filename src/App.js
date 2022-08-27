dbc968f656eaf27d6a7b04d6d4f57034df1c0b8a
Dhawan <dhawansolanki2103@gmail.com>
2022-08-27 16:12:51 +0530

    [Landing]

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import { Home, Auth } from "./components"
 function App() {
   return (
     <BrowserRouter>
-      <h1>Welcome to Lib!</h1>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="auth" element={<Auth />} />
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,263 @@
 import React from 'react'
-
+import './index.css';
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+       <div class="flex justify-center  ">
+       <nav class="self-center w-full max-w-7xl  ">
+                  <div class="flex flex-col lg:flex-row justify-around items-center border-b-2">
+                      <h1 class="uppercase pl-5 py-4 text-xl font-sans font-bold">HUMANITY</h1>
+                      <ul class="hidden lg:flex items-center text-[18px] font-semibold pl-32">
+                          <li class="hover:underline  underline-offset-4 decoration-2 decoration-black py-2 rounded-lg px-5">
+                              <a href="#">Home</a>
+                          </li>
+                          <li class="hover:underline underline-offset-4 decoration-2 decoration-black py-2 rounded-lg px-5"><a
+                                  href="#">Contact</a></li>
+                          <li class="hover:underline underline-offset-4 decoration-2 decoration-black py-2 rounded-lg px-5"><a
+                                  href="#">Services</a></li>
+                          <li class="hover:underline underline-offset-4 decoration-2 decoration-black py-2 rounded-lg px-5"><a
+                                  href="#">About</a></li>
+                          <li class="hover:underline underline-offset-4 decoration-2 decoration-black py-2 rounded-lg px-5"><a
+                                  href="#">Pricing</a></li>
+                                  
+
+                      </ul>
+                      <ul class="hidden lg:flex items-center text-[18px] font-semibold pl-32">
+                      <button class="button-31" role="button">Login</button>
+                      </ul>
+                      <div class=" text-center text-base pr-5  inline-flex"> <a href="#"
+                              class="w-8 h-8 inline-block rounded-full pt-[6px] hover:text-blue-500"><i
+                                  class="fa fa-twitter"></i></a> <a href="#"
+                              class="w-8 h-8 inline-block rounded-full pt-[5px] hover:text-blue-500"><i
+                                  class="fa fa-instagram"></i></a> <a href="#"
+                              class="w-8 h-8 inline-block rounded-full pt-[5px] hover:text-blue-500"><i
+                                  class="fa fa-facebook"></i></a> <a href="#"
+                              class="w-8 h-8 inline-block rounded-full pt-[5px] hover:text-blue-500"><i
+                                  class="fa fa-google"></i></a> <a href="#"
+                              class="w-8 h-8 inline-block rounded-full pt-[5px] hover:text-blue-500"><i
+                                  class="fa fa-linkedin"></i></a> </div>
+                  </div>
+              </nav>
+          </div>
+                                        <div class="flex justify-center ">
+                                            <div class="flex flex-col justify-center">
+                                    
+                                                <div class="flex flex-col max-w-7xl justify-center items-center p-2 mt-10">
+                                                    <div class="flex flex-col w-[90%] md:w-3/4 text-center items-center justify-center space-y-3">
+                                                        {/* <div class="text-5xl font-bold ">
+                                                            HUManity
+                                                        </div> */}
+                                                        {/* <div class="text-xl font-semibold text-gray-400 ">
+                                                            Humanity Uplifting Mankind
+                                                        </div> */}
+                                                        {/* <div class="md:text-lg"> 
+                                                            <input
+                                                                class="bg-gray-200/70 border drop-shadow-sm shadow-gray-700 rounded-2xl placeholder-slate-400 px-4 py-1 focus:outline-none "
+                                                                type="email" placeholder="E-mail Address" /> 
+                                                                <button
+                                                                class="p-1 m-2 rounded-2xl text-white bg-blue-500 px-4 font-semibold  hover:bg-blue-900 hover:trandform ease-in-out duration-300">
+                                                                Subscribe </button> 
+                                                            </div> */}
+                                                    </div>
+                                                    {/* <div class="w-3/4 h-60 md:h-96 my-4  rounded-lg overflow-hidden">
+                                                        <img src="https://source.unsplash.com/500x400/?cryptocurrency,electricity" class="h-full w-full" alt="" />
+                                                    </div> */}
+                                                </div>
+                                                
+                                            </div>
+                                        </div>
+
+                                        <div class="flex justify-center ">
+                                            
+                                        </div>
+
+
+{/* ------------------------------------------------------------------ */}
+                                        <div class="flex justify-center  ">
+                                            <div class="flex flex-col items-center justify-center">
+                                    
+                                                <div class="flex flex-col  max-w-7xl justify-center items-center space-y-3 w-full ">
+                                                    <div class="flex flex-col   md:items-start items-center justify-center  space-y-3 px-8 text-center ">
+                                                        <div class="text-3xl md:text-7xl font-bold ">
+                                                        Humanity Uplifting Mankind</div>
+                                                    </div>
+                                                    <div
+                                                        class="flex flex-col lg:flex-row space-x-2 space-y-3 md:space-x-6   w-full items-center justify-center ">
+                                    
+                                                        <div class="lg:w-40 w-64 h-40  overflow-hidden rounded-xl ">
+                                                            <img src="https://source.unsplash.com/random/300x500/?education
+                                    
+                                                            " alt="" class="" />
+                                                        </div>
+                                                        <div class="flex flex-row lg:flex-col space-x-3 space-y-6 items-center justify-center">
+                                                            <div class="w-32 lg:w-40 h-32  overflow-hidden rounded-xl ">
+                                                                <img src="https://source.unsplash.com/random/300x500/?books
+                                    
+                                                                " alt="" class="" />
+                                                            </div>
+                                                            <div class="w-32 lg:w-40 h-48  overflow-hidden rounded-xl ">
+                                                                <img src="https://source.unsplash.com/random/300x500/?computer
+                                    
+                                                                " alt="" class="" />
+                                                            </div>
+                                                        </div>
+                                                        <div class="lg:w-60 w-64 h-96  overflow-hidden rounded-xl ">
+                                                            <img src="https://source.unsplash.com/random/300x500/?library
+                                    
+                                                            " alt="" class="" />
+                                                        </div>
+                                                        <div class="flex flex-row lg:flex-col space-x-3 space-y-6 items-center justify-center ">
+                                                            <div class="w-32 lg:w-40 h-48  overflow-hidden rounded-xl ">
+                                                                <img src="https://source.unsplash.com/random/300x500/?reading
+                                    
+                                                                " alt="" class="" />
+                                                            </div>
+                                                            <div class="w-32 lg:w-40 h-32  overflow-hidden rounded-xl ">
+                                                                <img src="https://source.unsplash.com/random/300x500/?writing
+                                    
+                                                                " alt="" class="" />
+                                                            </div>
+                                                        </div>
+                                                        <div class="lg:w-40 w-64 h-40  overflow-hidden rounded-xl ">
+                                                            <img src="https://source.unsplash.com/random/300x500/?man
+                                    
+                                                            " alt="" class="" />
+                                                        </div>
+                                    
+                                    
+                                                    </div>
+                                    
+                                                </div>
+                                            </div>
+                                        </div> 
+< br />< br />
+                                        <div class="flex space-x-2 justify-center">
+                                        <button onclick="location.href=https://www.google.com" class="name noselect">Start Your New Reading Journey</button>
+</div>
+
+{/* ----------------------------------------------------------------------- */}
+                                        <script src="https://cdn.tailwindcss.com"></script>
+                                        <script src="https://use.fontawesome.com/03f8a0ebd4.js"></script>
+                                    
+                                        <footer class="text-gray-600 body-font">
+  <div class="container px-5 py-24 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
+    <div class="w-64 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left">
+      <a class="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
+          <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+        </svg>
+        <span class="ml-3 text-xl">Tailblocks</span>
+      </a>
+      <p class="mt-2 text-sm text-gray-500">Air plant banjo lyft occupy retro adaptogen indego</p>
+    </div>
+    <div class="flex-grow flex flex-wrap md:pl-20 -mb-10 md:mt-0 mt-10 md:text-left text-center">
+      <div class="lg:w-1/4 md:w-1/2 w-full px-4">
+        <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
+        <nav class="list-none mb-10">
+          <li>
+            <a class="text-gray-600 hover:text-gray-800">First Link</a>
+          </li>
+          <li>
+            <a class="text-gray-600 hover:text-gray-800">Second Link</a>
+          </li>
+          <li>
+            <a class="text-gray-600 hover:text-gray-800">Third Link</a>
+          </li>
+          <li>
+            <a class="text-gray-600 hover:text-gray-800">Fourth Link</a>
+          </li>
+        </nav>
+      </div>
+      <div class="lg:w-1/4 md:w-1/2 w-full px-4">
+        <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
+        <nav class="list-none mb-10">
+          <li>
+            <a class="text-gray-600 hover:text-gray-800">First Link</a>
+          </li>
+          <li>
+            <a class="text-gray-600 hover:text-gray-800">Second Link</a>
+          </li>
+          <li>
+            <a class="text-gray-600 hover:text-gray-800">Third Link</a>
+          </li>
+          <li>
+            <a class="text-gray-600 hover:text-gray-800">Fourth Link</a>
+          </li>
+        </nav>
+      </div>
+      <div class="lg:w-1/4 md:w-1/2 w-full px-4">
+        <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
+        <nav class="list-none mb-10">
+          <li>
+            <a class="text-gray-600 hover:text-gray-800">First Link</a>
+          </li>
+          <li>
+            <a class="text-gray-600 hover:text-gray-800">Second Link</a>
+          </li>
+          <li>
+            <a class="text-gray-600 hover:text-gray-800">Third Link</a>
+          </li>
+          <li>
+            <a class="text-gray-600 hover:text-gray-800">Fourth Link</a>
+          </li>
+        </nav>
+      </div>
+      <div class="lg:w-1/4 md:w-1/2 w-full px-4">
+        <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
+        <nav class="list-none mb-10">
+          <li>
+            <a class="text-gray-600 hover:text-gray-800">First Link</a>
+          </li>
+          <li>
+            <a class="text-gray-600 hover:text-gray-800">Second Link</a>
+          </li>
+          <li>
+            <a class="text-gray-600 hover:text-gray-800">Third Link</a>
+          </li>
+          <li>
+            <a class="text-gray-600 hover:text-gray-800">Fourth Link</a>
+          </li>
+        </nav>
+      </div>
+    </div>
+  </div>
+  <div class="bg-gray-100">
+    <div class="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
+      <p class="text-gray-500 text-sm text-center sm:text-left">© 2020 Tailblocks —
+        <a href="https://twitter.com/knyttneve" rel="noopener noreferrer" class="text-gray-600 ml-1" target="_blank">@knyttneve</a>
+      </p>
+      <span class="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
+        <a class="text-gray-500">
+          <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+            <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+          </svg>
+        </a>
+        <a class="ml-3 text-gray-500">
+          <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+            <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+          </svg>
+        </a>
+        <a class="ml-3 text-gray-500">
+          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+            <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+            <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+          </svg>
+        </a>
+        <a class="ml-3 text-gray-500">
+          <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="0" class="w-5 h-5" viewBox="0 0 24 24">
+            <path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"></path>
+            <circle cx="4" cy="4" r="2" stroke="none"></circle>
+          </svg>
+        </a>
+      </span>
+    </div>
+  </div>
+</footer>                                
+
+    </div>
+
+    
+    
   )
 }
 
